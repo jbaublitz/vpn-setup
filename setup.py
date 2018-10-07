da9b40ddc7f120bb4c5a470d5ce323ab34ff9928
John Baublitz <john.m.baublitz@gmail.com>
2018-10-07 16:40:33 -0400

    Initial code commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+import setuptools
+
+setup(name="vpn-setup",
+      version="0.1.0",
+      description="Automated setup for VPNs on Digital Ocean",
+      author="John Baublitz",
+      author_email="john.m.baublitz@gmail.com",
+      url="https://github.com/jbaublitz/vpn-setup",
+      scripts=["vpnsetup.py"],
+      packages=["digitalocean", "vpn"],
+      install_requires=[
+          'fabric',
+      ])
